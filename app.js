0287b516af60d65e6edf5474dff2fb32e84f33c8
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-06 18:13:23 +0800

    add Event Listener from addBtn

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// 程式碼寫這裡
+document.addEventListener("DOMContentLoaded", () => {
+  const taskInput = document.querySelector("#taskInput");
+  const addBtn = document.querySelector("#addBtn");
+
+  addBtn.addEventListener("click", () => {
+    console.log("Make sure that listening event can be catched from addBtn");
+  });
+});
