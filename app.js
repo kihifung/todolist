e6efb0236bbdf135f9fd8bda30526da7424063f1
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-07 11:17:18 +0800

    add todo-Item to todo-List

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskInput = document.querySelector("#taskInput");
   const addBtn = document.querySelector("#addBtn");
+  const todoList = document.querySelector(".todo-list");
+
+  const createTaskItem = () => {
+    const task = taskInput.value;
+    const todoItem = `<li class="todo-item">
+            <span class="item">${task}</span>
+            <button class="closeBtn">X</button>
+          </li>`;
+    todoList.insertAdjacentHTML("afterbegin", todoItem);
+    taskInput.value = ""; // 新增完項目之後，清空輸入框
+    taskInput.focus(); // 新增完項目之後，讓游標回到輸入框
+  };
 
   addBtn.addEventListener("click", () => {
-    console.log("Make sure that listening event can be catched from addBtn");
+    console.log(createTaskItem());
   });
 });
